fe3c47c3af741b5fa05d9dcde04873f1f7be4111
verebes.tibor <verebes.tibor@28-DIAK9>
2024-10-07 12:46:12 +0200

    Add project files.

---
+++ Verebes_Herkules_Keszthely/Program.cs
@@ -0,0 +1,32 @@
+﻿using Verebes_Herkules_Keszthely;
+using System.Text;
+
+List<Versenyzok> versenyzoklista = [];
+
+using StreamReader sr = new(path: @"..\..\..\src\forras.txt");
+while(!sr.EndOfStream) versenyzoklista.Add(new(sr.ReadLine()));
+
+Console.WriteLine($"{versenyzoklista.Count} versenyző fejezte be a versenyt.");
+
+//1.feladat
+var lnelitj = versenyzoklista.Count(p => p.Kategoria == "elit junior");
+Console.WriteLine($"1. feladat: Versenyzők száma elit junior kategóriában: {lnelitj}");
+
+//2.fealdat
+var lnferfik = versenyzoklista.Where(p => p.Nem == "f");
+var lnfatlag = lnferfik.Average(p => p.Eletkor);
+Console.WriteLine($"2. feladat: férfi versenyzők átlagéletkora: {lnfatlag:0} év");
+
+//3.feladat
+var lnfutas = versenyzoklista.Sum(p => p.Futas.TotalHours);
+Console.WriteLine($"3. feladat: a versenyzők futással töltött összideje órában: {lnfutas:0.00}");
+
+//4.feladat
+var lnhuszhuszonnegy = versenyzoklista.Where(p => p.Kategoria == "20-24");
+var lnuszasatlag = lnhuszhuszonnegy.Average(p => p.Uszas.TotalHours + (p.Uszas.TotalMinutes / 60) + (p.Uszas.TotalSeconds / 360));
+Console.WriteLine($"4. feladat: átlagos úszási idő 20-24 kategóriában: {lnuszasatlag:0} óra");
+
+//5.feladat
+var lnnok = versenyzoklista.Where(p => p.Nem == "n");
+
+//6.feladat
---
+++ Verebes_Herkules_Keszthely/Versenyzok.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verebes_Herkules_Keszthely
+{
+    internal class Versenyzok
+    {
+        public string Nev { get; set; }
+        public int Ev { get; set; }
+        public int Rajtszam { get; set; }
+        public string Nem { get; set; }
+        public string Kategoria { get; set; }
+        public TimeSpan Uszas { get; set; }
+        public TimeSpan Elso { get; set; }
+        public TimeSpan Kerekpar { get; set; }
+        public TimeSpan Masodik { get; set; }
+        public TimeSpan Futas {  get; set; }
+        public int Eletkor => (int)(2024 - Ev);
+        public override string ToString()
+        {
+            return $"{Nev}:{Ev}:{Rajtszam}:{Nem}:{Kategoria}:{Uszas}:{Elso}:{Kerekpar}:{Masodik}:{Futas}";
+        }
+
+        public Versenyzok(string row) 
+        {
+            var v = row.Split(';');
+            Nev = v[0];
+            Ev = int.Parse(v[1]);
+            Rajtszam = int.Parse(v[2]);
+            Nem = v[3];
+            Kategoria = v[4];
+            Uszas = TimeSpan.Parse(v[5]);
+            Elso = TimeSpan.Parse(v[6]);
+            Kerekpar = TimeSpan.Parse(v[7]);
+            Masodik = TimeSpan.Parse(v[8]);
+            Futas = TimeSpan.Parse(v[9]);
+        }
+    }
+}
